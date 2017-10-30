0b9a7bbe193b0e946267682502c6a120f0e4ea1d
timofonic <timofonic@gmail.com>
2017-10-30 04:16:45 +0100

    Another easy app. This made me investigate a bit about Unicode on Java. Please check the validity of the code.
    Signed-off-by: timofonic <timofonic@gmail.com>

---
+++ src/SpecialCharacters.java
@@ -0,0 +1,11 @@
+import static java.lang.System.out;
+
+/* 1.3 - Special characters
+* * It shows some special characters (Unicode).
+* @author Timofonic
+*/
+public class SpecialCharacters {
+    public static void main(String[] args) {
+        System.out.println("When I code using the \ud83d\uddae of my \ud83d\uddb3, I get \ud83d\ude00 like a \ud83e\uddd2");
+    }
+}
